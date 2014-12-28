dce2facc8429deb0bd10131c63ee82ab768703eb
Mike <ccteym@gmail.com>
2014-12-28 08:08:20 +0800

    framework

---
+++ Example/SideMenu/SideMenu.h
@@ -0,0 +1,19 @@
+//
+//  SideMenu.h
+//  SideMenu
+//
+//  Created by Mike on 14/12/28.
+//  Copyright (c) 2014年 Evgeny Nazarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SideMenu.
+FOUNDATION_EXPORT double SideMenuVersionNumber;
+
+//! Project version string for SideMenu.
+FOUNDATION_EXPORT const unsigned char SideMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SideMenu/PublicHeader.h>
+
+
